fcb1fc1b621ba1b1e9f388c690fddd82aafbbcf1
Aralí Mata <aralipdf@gmail.com>
2022-07-28 22:38:18 -0500

    Main functionalities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import { MdFingerprint } from 'react-icons/md';
+import {GiEarthAmerica} from 'react-icons/gi';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import {Button} from './Button';
 import './Navbar.css';
@@ -34,8 +35,8 @@ function Navbar() {
     <div className="navbar">
      <div className="navbar-container container">
         <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-            <MdFingerprint className='navbar-icon'/>
-            LAVISH
+            <GiEarthAmerica color="#f00946" className='navbar-icon'/>
+            EarthquakeSearch
         </Link>
         <div className="menu-icon" onClick={handleClick}>
         {click ? <FaTimes /> : <FaBars/>}
@@ -51,24 +52,7 @@ function Navbar() {
                     Search History
                 </Link>
             </li>
-            <li className="nav-item">
-                <Link to='/products'className="nav-links" onClick={closeMobileMenu}>
-                    Products
-                </Link>
-            </li>
-            <li className="nav-btn">
-                {button ? (
-                    <Link to="/sign-up" className="btn-link" >
-                        <Button buttonStyle='btn--outline'>SIGN UP</Button>
-                    </Link>
-                ) : (
-                    <Link to="/sign-up" className="btn-link" onClick={closeMobileMenu}>
-                         <Button buttonStyle='btn--outline' 
-                         buttonSize='btn--mobile'>SIGN UP</Button>
-                    </Link>
-                )
-                }
-            </li>
+        
         </ul>
      </div>
     </div>
