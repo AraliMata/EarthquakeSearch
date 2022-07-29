791f53ef562d5cc18edbf02f0e237872cc88c0d4
Aralí Mata <aralipdf@gmail.com>
2022-07-28 23:00:24 -0500

    node server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/pages/HomePage/Home';
+import Home from './components/pages/Home';
 import Footer from './components/Footer';
 import History from './components/pages/History';
 
